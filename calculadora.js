4851dd973e42b5156bc3558a57c813b8fc93d840
joao <joaoeduardo.lsilva@gmail.com>
2023-05-15 20:22:36 -0300

    primeiro test

---
+++ calculadora.js
@@ -0,0 +1,19 @@
+function somar(a, b) {
+  return a + b;
+}
+exports.somar = somar;
+
+function multiplica(a, b) {
+  return a * b;
+}
+exports.multiplica = multiplica;
+
+function dividir(a, b) {
+  return a / b;
+}
+exports.dividir = dividir;
+
+function subtrair(a, b) {
+  return a - b;
+}
+exports.subtrair = subtrair;
